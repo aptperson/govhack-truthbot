24bc82756e6fa183a3c2df61735178da00d6cb59
Andy Kitchen <kitchen.andy+git@gmail.com>
2016-07-30 17:36:08 +1000

    better model stuff

--- model/process_tweet_data.py
+++ model/process_tweet_data.py
@@ -6,17 +6,19 @@ from collections import Counter
 import pandas as pd
 import numpy as np
 
+unk_word = '<UNK>'
+start_word = '<START>'
+
 tweets = pd.concat(pd.read_csv(f, dtype={'id': np.int64}) for f in glob.glob('../tweet_data/*.csv'))
 
 texts = tweets.text.values
 
 c = Counter(tok for text in texts for tok in text.split())
 
-vocab = [w for w, n in c.most_common() if n > 50]
-vocab.append('UNK')
+vocab = [start_word] + [w for w, n in c.most_common() if n > 50] + [unk_word]
 
-unk_id = len(vocab) - 1
 word_to_vocab = {w: i for i, w in enumerate(vocab)}
+unk_id = word_to_vocab[unk_word]
 
 tweets['tokenized'] = tweets.text.apply(lambda t: [word_to_vocab.get(w, unk_id) for w in t.split()])
 
