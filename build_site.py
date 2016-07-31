49001f1aa5f10937b330104804a66c0bd275f061
Andy Kitchen <kitchen.andy+git@gmail.com>
2016-07-31 12:21:59 +1000

    added more info

--- build_site.py
+++ build_site.py
@@ -1,6 +1,7 @@
 import os
 import pandas
 import pystache
+import pickle
 
 with open('list.html.mustache') as f:
 	list_template = f.read()
@@ -8,8 +9,17 @@ with open('list.html.mustache') as f:
 with open('poli.html.mustache') as f:
 	poli_template = f.read()
 
-with open('friends.txt') as f:
-	poli_list = [{'name': name.rstrip(), 'url': 'poli/%s.html' % name.rstrip()} for name in f.readlines()]
+# with open('friends.txt') as f:
+# 	poli_list = [{'name': name.rstrip(), 'url': 'poli/%s.html' % name.rstrip()} for name in f.readlines()]
+
+with open('friends_info.pkl', 'rb') as f:
+	friends_info = pickle.load(f)
+
+def add_url(poli):
+	poli['url'] = 'poli/%s.html' % poli['screen_name']
+	return poli
+
+poli_list = [add_url(poli) for poli in friends_info]
 
 list_page = pystache.render(list_template, {'polis': poli_list})
 
